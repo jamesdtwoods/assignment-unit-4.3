08e4d1bbd63fdaaa76d28c82ebad7938f1f881a6
James Woods <james.d.t.woods@gmail.com>
2023-10-13 14:53:51 -0500

    finished with the first 3 stretch functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,16 +4,29 @@ console.log('***** Cart Functions *****');
 
 let basket = [];
 
+// function addItem(item) {
+//     console.log('in add item:', item);
+//     basket.push(item);
+//     return true;
+// }
+
 function addItem(item) {
     console.log('in add item:', item);
-    basket.push(item);
-    return true;
+    if (!isFull()) {
+        basket.push(item);
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
+console.log(`Basket is ${basket}`);
 console.log('running addItem:', addItem('item1'));
 console.log('running addItem:', addItem('item2'));
 console.log('running addItem:', addItem('item3'));
 console.log('running addItem:', addItem('item4'));
+console.log(`Basket is now ${basket}`);
 
 
 function listItems() {
@@ -27,15 +40,37 @@ console.log('running listItems:', listItems());
 
 function empty() {
     console.log('in empty')
-    for (let i = 0; i < basket.length; i++) {
+    for (let i = basket.length; i = basket.length; i--) {
         console.log('in empty loop:', basket[i]);
         basket.pop();
     }
 }
 
+console.log(`Basket is ${basket}`);
 console.log('running empty:', empty());
+console.log(`Basket is now ${basket}`);
 
-console.log(basket);
+const maxItems = 5;
+
+function isFull() {
+    if (basket.length < 5 && basket.length >= 0){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+console.log('is the basket full?', isFull());
+
+
+
+
+
+
+
+
+
 
 // DO NOT MODIFY
 // Used for automated testing
